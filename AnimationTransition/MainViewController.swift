232b323b5e4621ed7a4af7d1c0130e70bcfe2472
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-06-23 10:23:16 +0200

    Add secondary view controller presented modally.
    The secondary view controller is dismissed with an unwind segue.

---
+++ AnimationTransition/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AnimationTransition
+//
+//  Created by Jorge D. Ortiz Fuentes on 23/6/15.
+//  Copyright (c) 2015 PoWWaU. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBAction func unwindSegueAction(sender: UIStoryboardSegue) {
+    }
+}
